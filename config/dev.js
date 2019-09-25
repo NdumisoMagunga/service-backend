435e71e8e8923ee6398b21d69e3fd2c0eaad5f0a
NdumisoMagunga <magunga55gmail.com>
2019-09-25 14:54:43 +0100

    first commit

--- config/dev.js
+++ config/dev.js
@@ -1,5 +1,5 @@
 module.exports = {
     "database" : 'mongodb+srv://Ndumiso:Focus12451@rating-app-eiytj.mongodb.net/test?retryWrites=true&w=majority',
-    "port" : process.env.PORT || 80,
+    "port" : process.env.PORT || 5000,
     "secret" : 'Focus12451',
 }
